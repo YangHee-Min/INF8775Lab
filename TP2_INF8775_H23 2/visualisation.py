10d694a7818b6c6dc0cd89f31b4470ac009f7f92
Hee-MinYang <hee-min.yang@polymtl.ca>
2023-03-27 21:24:13 -0400

    clean up visualization

--- TP2_INF8775_H23 2/visualisation.py
+++ TP2_INF8775_H23 2/visualisation.py
@@ -1,9 +1,5 @@
 import math
-import sys
 from PIL import Image, ImageDraw
-# from algo_glouton import TSP as TSP_GLOUTON
-# from algo_mst import TSP as TSP_MST
-# from algo_dynamique import TSP as TSP_DYN
 
 
 def get_coords(file_name):
@@ -16,27 +12,6 @@ def get_coords(file_name):
         line = f.readline()
         coords.append(tuple(int(coord) for coord in line.split("  ")))
     return coords
-
-# Create visual support
-
-
-def draw_points(path, filename):
-    img = Image.new('RGB', (2000+2, 2000+2), (255, 255, 255))
-    draw = ImageDraw.Draw(img)
-
-    for i, coord in enumerate(path):
-        # print point
-        x1, y1 = coord
-        # create Point
-        draw.point((x1 + 1, y1 + 1), fill=(0, 0, 0))
-        # print path to next
-        if i < len(path)-1:
-            x2, y2 = path[i+1]
-            # create line
-            draw.line((x1+1, y1+1, x2+1, y2+1), fill=(0, 0, 0), width=1)
-
-    # Save Image
-    img.save(f"{filename}.png")
 
 
 def get_cost_path(optimal_path_coords: list):
@@ -61,30 +36,3 @@ def get_path(coords: list, optimal_path: list):
     if (before_last_index < second_index):
         index_path.reverse()
     return index_path
-
-
-if __name__ == "__main__":
-    filename = "./examples/N5_0"
-    # coords = get_coords(filename)
-    # list_greedy = TSP_GLOUTON(coords)
-    # print(get_cost_path(list_greedy))
-
-    coords = get_coords(filename)
-    # list_mst = TSP_MST(coords)
-    draw_points(list_mst, "testMST")
-    print(get_cost_path(list_mst))
-
-    # file_name = "N15_"
-    # for i in range(5):
-    #     file = file_name + str(i)
-    #     draw_points(TSP_GLOUTON(get_coords(file)), file+"_GLOUTON")
-    # file_name = "N50_"
-    # for j in range(5):
-    #     file = file_name + str(j)
-    #     draw_points(TSP_MST(get_coords(file)), file+"_TSP")
-    """
-    file_name = "N15_"
-    for k in range(1):
-        file= file_name + str(j)
-        draw_points(TSP_DYN(get_coords(file)), file+"_DYNAMIQUE")
-    """
