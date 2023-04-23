c1ecc70d7edd994d3abbc57ad79b09af068ab134
Hee-MinYang <hee-min.yang@polymtl.ca>
2023-04-23 19:41:57 -0400

    solved bug where it would put it at spot 5,5

--- tp3/TP3-H23/gen_enc.py
+++ tp3/TP3-H23/gen_enc.py
@@ -23,7 +23,6 @@ def generate_enclosure(start_row, start_column, enclosure_id, current_size, max_
     # find a way to keep all of the points and
 
     start_coords = (start_row, start_column)
-    enclosure_coords = []
 
     (row, col) = start_coords
     while current_size < max_size:
@@ -31,7 +30,7 @@ def generate_enclosure(start_row, start_column, enclosure_id, current_size, max_
             print(
                 f'overwriting {table[row][col]} with {enclosure_id}')
 
-        table[row][col] = enclosure_id
+        table[row][col] = current_size
         current_size += 1
         if current_size == max_size:
             return table
@@ -42,36 +41,71 @@ def generate_enclosure(start_row, start_column, enclosure_id, current_size, max_
         while len(possible_new_coords) < 1:
             # center table in a bigger table if we're trying to go to a border
             if row == 0 or row == len(table) - 1 or col == 0 or col == len(table[0]) - 1:
+                # print("double size")
                 table, row, col = center_new_table(table, row, col)
                 possible_new_coords = get_possible_next_coords(table, row, col)
             # choose a random point amongst the points of our already existing points to continue off of in hopes it is an edge
-            # TODO: replace with function to find edges of
             else:
-                row, col = enclosure_coords[random.randint(
-                    0, len(enclosure_coords))]
-                possible_new_coords = get_possible_next_coords(
-                    table, row, col)
+                # print("stuck")
+                # print_table(table)
+                border_points = get_border(table, enclosure_id)
+                row, col = border_points[random.randint(
+                    0, len(border_points) - 1)]
+                possible_new_coords = get_possible_next_coords(table, row, col)
 
         new_coords_index = 0 if len(possible_new_coords) == 1 else random.randint(
             0, len(possible_new_coords) - 1)
+        # if get_manhattan_distance((row, col), possible_new_coords[new_coords_index]) > 1:
+        # print('\033[93m' + "AAAAAAAAAAAAAAH" + '\033[0m')
+        # print_table(table)
+        # print(
+        # f'current: ({row}, {col}) {possible_new_coords} index:{new_coords_index}')
         row, col = possible_new_coords[new_coords_index]
-        enclosure_coords.append((row, col))
     return table
+
+
+def get_manhattan_distance(tuple1, tuple2):
+    return abs(tuple2[0] - tuple1[0]) + abs(tuple2[1] - tuple1[1])
+
+
+def get_border(table, value):
+    rows, cols = len(table), len(table[0])
+    border = []
+
+    # Check each element and its neighbors
+    for i in range(rows):
+        for j in range(cols):
+            if table[i][j] == value:
+                if i == 0 or i == rows-1 or j == 0 or j == cols-1:
+                    # Element is on the edge of the array
+                    border.append((i, j))
+                else:
+                    # Check if any neighbors are not the target value
+                    if table[i-1][j] != value or table[i+1][j] != value or table[i][j-1] != value or table[i][j+1] != value:
+                        border.append((i, j))
+
+    return border
 
 
 def get_possible_next_coords(table, row, col):
     possible_new_coords = []
     for direction in Direction:
-        next_coords = get_next_cell_coords(direction, row, col)
+        next_coords = get_next_cell_coords(
+            direction, row, col, len(table[0]), len(table))
         if is_cell_free(table, next_coords[0], next_coords[1]):
             possible_new_coords.append(next_coords)
+        # print(f'current: ({row}, {col}) {possible_new_coords}')
     return possible_new_coords
 
 
 def center_new_table(table, row, col):
-    row, col = get_centered_coordinates(table, row, col)
+    # print(f'old:{row}, {col}')
+    # print_table(table)
+    new_row, new_col = get_centered_coordinates(table, row, col)
+    # print(f'new:{new_row}, {new_col}')
     table = center_table_and_double_size(table)
-    return (table, row, col)
+    # print_table(table)
+    return (table, new_row, new_col)
 
 
 def center_table_and_double_size(table):
@@ -114,15 +148,15 @@ def is_cell_free(table, row: int, col: int):
     return True
 
 
-def get_next_cell_coords(direction: Union[Direction, int], row: int, column: int):
+def get_next_cell_coords(direction: Union[Direction, int], row: int, column: int, width: int, height: int):
     if direction == Direction.LEFT:
         return (row, column - 1 if column - 1 >= 0 else 0)
     elif direction == Direction.UP:
         return (row - 1 if row - 1 >= 0 else 0, column)
     elif direction == Direction.RIGHT:
-        return (row, column + 1 if column + 1 < TABLE_WIDTH else TABLE_WIDTH)
+        return (row, column + 1 if column + 1 < width else width)
     elif direction == Direction.DOWN:
-        return (row + 1 if row + 1 < TABLE_HEIGHT else TABLE_HEIGHT, column)
+        return (row + 1 if row + 1 < height else height, column)
 
 
 def generate_random_direction():
